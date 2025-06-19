e4f9b1f89158ed2b041d2fa8ca9821eb3e883b0d
tranhayem <mailcuathy@gmail.com>
2025-06-19 11:01:48 +0700

    chore: update Tailwind content paths to include src/modules

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
     "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/modules/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
